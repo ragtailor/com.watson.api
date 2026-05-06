b5defd438ce1d813320baa69f7d9941afcb783f9
dpthf001107 <dpthf001107@gmail.com>
2026-05-06 16:48:32 +0900

    아고라 추가

---
+++ apps/agora/app/zeno_of_citium.py
@@ -0,0 +1,34 @@
+
+'''
+키티온의 제논 (Zeno of Citium): 아고라에 있는 '포이킬레 스토아(채색 주랑)'에서 
+제자들을 가르쳤습니다. 
+이 건물 이름에서 그 유명한 스토아학파라는 명칭이 유래되었습니다.
+'''
+
+class ZenoOfCitium:
+
+    def __init__(self):
+        pass
+
+
+'''
+amyshin.dev 🎵
+anjgkwl.com 🚪
+kimseunga.art 🫦
+bestcow.cloud 🐮
+iyungyeong8088.cloud 👩🏻
+cloverky.cloud 🍀
+codenameseol.cloud 💊
+eungsang.com 🐥
+foodopenlab.com🕕
+jsangho.cloud💫
+minahdev.cloud 🐱
+paiksunggum.com 🪸
+pmhllll12.cloud 🍎
+proteier.cloud 🥩
+redoceanmap.com ✈️ 
+seongyuna.cloud 🐛
+suvisdev.cloud 🤖
+whoareryu.cloud 🦁
+woojeongalex.cloud🐶
+'''
