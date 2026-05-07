312b49b192c90592adf3546437c686dfb107fc10
dpthf001107 <dpthf001107@gmail.com>
2026-05-07 15:08:21 +0900

    .

--- apps/titanic/app/james.py
+++ apps/titanic/app/james.py
@@ -12,17 +12,13 @@ class James:
 
 @app.get("/")
 def read_root():
-    return {"message": "제임스가 메인이다.", "docs": "/docs"}
+    return {"message": "FAST API 초기화 성공 ", "docs": "/docs"}
 
 
-@app.get("/data")
-def read_titanic_preview():
-    w = Walter()
-    return w.head_records()
+
 
 
 if __name__ == "__main__":
     import uvicorn
 
-    print("제임스가 메인이다. (uvicorn)")
     uvicorn.run("james:app", host="127.0.0.1", port=8000, reload=True)
